cbd6c4209943afc696d73590613bedd86c189857
Aleksandr Kondratskii <aleksandr.kondratskii@macys.com>
2024-03-09 03:40:46 +0100

    feat: Add Person class

---
+++ src/App.java
@@ -0,0 +1,53 @@
+import entity.Person;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class App {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public void run() {
+        System.out.println("Welcome to the Person Management System");
+        System.out.println("1. Input a person");
+        System.out.println("2. Load a person from file");
+
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                inputPerson();
+                break;
+            case 2:
+                loadPerson();
+            default:
+                System.out.println("Invalid choice");
+        }
+    }
+
+    private void loadPerson() {
+        System.out.println("Enter the file name:");
+        String fileName = scanner.next();
+        scanner.nextLine();
+        Person person = Person.loadFrom(new File(fileName));
+
+        if (person != null) {
+            System.out.println("Person loaded: " + person);
+        }
+    }
+
+    private void inputPerson() {
+        System.out.println("Enter the name:");
+        String name = scanner.next();
+        scanner.nextLine();
+        System.out.println("Enter the age:");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        Person person = new Person(name, age);
+        System.out.println("Input file name to save:");
+        String fileName = scanner.next();
+        scanner.nextLine();
+        File file = new File(fileName);
+        Person.saveTo(file, person);
+        System.out.println("Person created: " + person);
+    }
+}
--- src/entity/Person.java
+++ src/entity/Person.java
@@ -1,11 +1,35 @@
 package entity;
 
-public class Person {
+import java.io.*;
+
+public class Person implements Serializable {
     private final String name;
     private final int age;
 
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    public static void saveTo(File file, Person person) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
+            oos.writeObject(person);
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static Person loadFrom(File file) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+            return (Person) ois.readObject();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name + ", who is " + age + " years old.";
     }
 }

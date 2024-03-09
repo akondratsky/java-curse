30e8c3fcaab21f50404bd1eb62e8eca14dc8089b
Aleksandr Kondratskii <aleksandr.kondratskii@macys.com>
2024-03-09 03:27:16 +0100

    feat: Add Person class

---
+++ src/entity/Person.java
@@ -0,0 +1,11 @@
+package entity;
+
+public class Person {
+    private final String name;
+    private final int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}

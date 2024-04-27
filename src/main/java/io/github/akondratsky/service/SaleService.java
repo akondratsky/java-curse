d3466c06e323e4e6dc8dc5d593da46b3b646a7c0
Aleksandr Kondratskii <aleksandr.kondratskii@macys.com>
2024-04-27 22:41:55 +0200

    feat: task 16.11

--- src/main/java/io/github/akondratsky/service/SaleService.java
+++ src/main/java/io/github/akondratsky/service/SaleService.java
@@ -1,20 +1,40 @@
 package io.github.akondratsky.service;
 
+import io.github.akondratsky.entity.Currency;
 import io.github.akondratsky.entity.Sale;
 import io.github.akondratsky.repository.Repository;
 
 import java.util.List;
+import java.util.Map;
 
 public class SaleService {
     private final Repository<Sale> repository;
+    private final CurrencyService currencyService;
 
-    public SaleService(Repository<Sale> repository) {
+    public SaleService(Repository<Sale> repository, CurrencyService currencyService) {
         this.repository = repository;
+        this.currencyService = currencyService;
     }
 
     public List<Sale> loadAllByPersonId(int id) {
         return repository.loadAll().stream()
                 .filter(sale -> sale != null && sale.getId() == id)
                 .toList();
+    }
+
+    public double getAmount(Sale sale, Currency currency) {
+        double totalAmount = 0;
+
+        // iterate over currency amounts and sum
+        for (Map.Entry<Currency, Double> entry : sale.getAmount().entrySet()) {
+            Currency saleCurrency = entry.getKey();
+            double saleAmount = entry.getValue();
+            if (saleCurrency.equals(currency)) {
+                totalAmount += saleAmount;
+            } else {
+                totalAmount += currencyService.getRate(saleCurrency, currency) * saleAmount;
+            }
+        }
+        return totalAmount;
     }
 }
